0285b6d77874418f05196d3cb6e5ec0317e0b231
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-03-01 15:25:24 -0300

    aula_183

---
+++ models/Photo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+
+const photoSchema = new Schema({
+  image: String,
+  title: String,
+  likes: Array,
+  coments: Array,
+  userId: mongoose.isObjectIdOrHexString,
+  userName: String,
+},
+{
+  timestamps: true
+}
+);
