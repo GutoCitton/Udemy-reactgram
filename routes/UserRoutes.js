44a2b0c42acaf77052e3cbf77b01e748d2e9b5dc
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-03-07 15:42:48 -0300

    aula_191

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 // controller
 
-const { register, login } = require('../controllers/userController');
+const { register, login, getCurrentUser } = require('../controllers/userController');
 
 // Middlewares
 const validate = require('../middlewares/handleValidation')
 const { userCreateValidation, loginValidation } = require('../middlewares/userValidation')
+const authGuard = require('../middlewares/authGuard');
 
 // Routes
 router.post('/register',userCreateValidation(), validate, register);
 router.post('/login',loginValidation(), validate, login);
+router.get('/profile', authGuard, getCurrentUser)
 
 module.exports = router;
