cc16098d3216d11bfd8970c2828f9cab1f251148
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-07-18 14:12:35 -0300

    aula_227

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,37 +1,68 @@
-import './Navbar.css';
+import "./Navbar.css";
 
 // components
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link } from "react-router-dom";
 import {
   BsSearch,
   BsHouseDoorFill,
   BsFillPersonFill,
   BsFillCameraFill,
-} from 'react-icons/bs';
+} from "react-icons/bs";
+
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
   return (
-    <nav id='nav'>
-      <Link to='/'>ReactGram</Link>
-      <form id='search-form'>
+    <nav id="nav">
+      <Link to="/">ReactGram</Link>
+      <form id="search-form">
         <BsSearch />
-        <input type="text" placeholder='Pesquisar'/>
+        <input type="text" placeholder="Pesquisar" />
       </form>
-      <ul id='nav-links'>
-        <li>
-          <NavLink to='/'>
-            <BsHouseDoorFill />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login'>Entrar</NavLink>  
-        </li>
-        <li>
-          <NavLink to='/register'>Cadastrar</NavLink>
-        </li>
+      <ul id="nav-links">
+        {auth ? (
+          <>
+            <li>
+              <NavLink to="/">
+                <BsHouseDoorFill />
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink to={`/users/${user._id}`}>
+                  <BsFillCameraFill />
+                </NavLink>
+              </li>
+            )}
+            <li>
+              <NavLink tp='/profile'>
+                <BsFillPersonFill />
+              </NavLink>
+            </li>
+            <li>
+              <span>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login">Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/register">Cadastrar</NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
