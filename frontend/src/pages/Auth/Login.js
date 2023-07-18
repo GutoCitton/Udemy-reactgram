10e0a35f8c958c0cda600455f942744e6508bfe4
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-07-18 18:52:22 -0300

    aula_230

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,9 +1,47 @@
-import './Auth.css';
+import "./Auth.css";
+
+// Components
+import { Link, LinkProps } from "react-router-dom";
+import Message from "../../components/Message";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassowrd] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Faça o login para ver o que há de novo!</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassowrd(e.target.value)}
+          value={password || ""}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        não tem uma conta? <Link to="/register">Clique aqui!</Link>
+      </p>
+    </div>
+  );
+};
 
 export default Login;
