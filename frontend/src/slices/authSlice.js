cdceda1bbf98eea4f636af02a761f4259aba1425
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-07-18 14:17:05 -0300

    aula_228

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -1,7 +1,7 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import authService from '../services/authService';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "../services/authService";
 
-const user = JSON.parse(localStorage.getItem('user'));
+const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = {
   user: user ? user : null,
@@ -11,22 +11,27 @@ const initialState = {
 };
 
 // Register an user and sign in
-export const register = createAsyncThunk('auth/register', 
-  async(user, thunkAPI) => {
-
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
     const data = await authService.register(user);
 
     // Check for errors
     if (data.errors) {
-      return thunkAPI.rejectWithValue(data.errors[0])
+      return thunkAPI.rejectWithValue(data.errors[0]);
     }
 
     return data;
+  }
+);
 
+//Logout an user
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await authService.logout();
 });
 
 export const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
     reset: (state) => {
@@ -36,22 +41,30 @@ export const authSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(register.pending, (state) => {
-      state.loading = true;
-      state.error = false;
-    }).addCase(register.fulfilled, (state, action) => {
-      state.loading = false;
-      state.success = true;
-      state.error = null;
-      state.user = action.payload;
-    })
-    .addCase(register.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.payload;
-      state.user = null;
-    });
+    builder
+      .addCase(register.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = null;
+      });
   },
 });
 
-export const {reset} = authSlice.actions;
+export const { reset } = authSlice.actions;
 export default authSlice.reducer;
