cec5fc0408bebbe389c3fa70bec2c9c411c6e1f2
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-07-19 14:39:00 -0300

    aula_231

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -30,7 +30,19 @@ const login = async(data) => {
  
   const config = requestConfig('POST', data);
 
- 
+  try {
+    const res = await fetch(api + '/users/login', config)
+                  .then((res) => res.json())
+                  .catch((err) => err)
+
+    if (res) {
+      localStorage.setItem('user', JSON.stringify(res))
+    }
+
+    return res;
+  } catch (error) {
+    console.log(error)
+  }
 
 }
 
@@ -38,6 +50,7 @@ const login = async(data) => {
 const authService ={
   register,
   logout,
+  login,
 }
 
 export default authService;
