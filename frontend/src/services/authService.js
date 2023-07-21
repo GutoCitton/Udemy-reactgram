d7e510578b6eda67f343560384ed655114184fed
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-07-21 14:57:06 -0300

    aula_233

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -9,48 +9,44 @@ const register = async (data) => {
       .then((res) => res.json())
       .catch((err) => err);
 
-      if(res) {
-        localStorage.setItem('user', JSON.stringify(res));
-      }
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
+    }
 
-      return res;
-
+    return res;
   } catch (error) {
     console.log(error);
   }
 };
 
-// Logout an user 
+// Logout an user
 const logout = () => {
-  localStorage.removeItem('user');
-}
+  localStorage.removeItem("user");
+};
 
 // Sign in an user
-const login = async(data) => {
- 
-  const config = requestConfig('POST', data);
+const login = async (data) => {
+  const config = requestConfig("POST", data);
 
   try {
-    const res = await fetch(api + '/users/login', config)
-                  .then((res) => res.json())
-                  .catch((err) => err)
+    const res = await fetch(api + "/users/login", config)
+      .then((res) => res.json())
+      .catch((err) => err);
 
-    if (res) {
-      localStorage.setItem('user', JSON.stringify(res))
+    if (res._id) {
+      localStorage.setItem("user", JSON.stringify(res));
     }
 
     return res;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
+};
 
-}
-
-
-const authService ={
+const authService = {
   register,
   logout,
   login,
-}
+};
 
 export default authService;
