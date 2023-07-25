80d0465a739759a529a0b295f1c2c61faa3560c2
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-07-25 16:41:13 -0300

    aula_253

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -3,14 +3,15 @@ import { api, requestConfig } from "../utils/config";
 // Publish an user photo
 const publishPhoto = async (data, token) => {
   const config = requestConfig("POST", data, token, true);
+
   try {
     const res = await fetch(api + "/photos", config)
-      .then((res) => res.json)
+      .then((res) => res.json())
       .catch((err) => err);
 
-      return res;
+    return res;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
