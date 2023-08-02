e0f7278cb7540db5a6ad8dba372dcef395243589
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-08-02 17:45:22 -0300

    aula_275

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -95,9 +95,11 @@ const comment = async (data, id, token) => {
   const config = requestConfig("PUT", data, token);
 
   try {
-    const res = await fetch(api + "/photo/comment/" + id, config)
+    const res = await fetch(api + "/photos/comment/" + id, config)
       .then((res) => res.json())
       .catch((err) => err);
+
+      return res;
   } catch (error) {
     console.log(error);
   }
