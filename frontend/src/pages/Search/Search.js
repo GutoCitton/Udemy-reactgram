fbdcf953152dfae91194ad3aba2b6e0546ddfcd4
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-08-03 15:22:17 -0300

    aula_280

---
+++ frontend/src/pages/Search/Search.js
@@ -0,0 +1,18 @@
+import './Search.css';
+
+// hooks
+import { useEffect } from 'react';
+import { UseSelector, useDispatch } from 'react-redux';
+import { useResetComponentMessage } from '../../hooks/useResetComponentMessage';
+
+// Components
+import LikeContainer from '../../components/LikeContainer';
+import PhotoItem from '../../components/PhotoItem';
+import { Link } from 'react-router-dom';
+
+ 
+const Search  = () => {
+  return <div>Search</div>
+}
+
+export default Search;
